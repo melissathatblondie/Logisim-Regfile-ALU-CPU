4e3ac4d9966185cc4b0c2f7e9811b089a4917fb3
jonathansun5 <jonathansun5@berkeley.edu>
2017-02-27 21:10:21 -0800

    Add files via upload

---
+++ test.s
@@ -0,0 +1,52 @@
+addi $s0, $zero, 1
+addi $s1, $zero, 2
+add $s2, $s0, $s1
+# j Label
+
+addi $s0, $s0, 50
+
+# Label:
+
+addi $s0, $zero, 8
+addi $s0, $zero, 5
+addi $s0, $zero, 4
+addi $s0, $zero, 3
+addi $s0, $zero, 2
+addi $s0, $zero, 1
+addi $s0, $zero, 0
+addi $s0, $zero, 11
+
+# add $s2, $s2, $s0
+
+
+
+
+
+
+# addi $s1, $s1, 1
+# addi $s2, $s1, 5
+# # addi $t1, $t1, 1
+# addi $s1, $s1, 10
+# # addi $t2, $t2, 5
+# subu $s1, $s1, $s0
+
+# beq $s1, $t2, Here
+# subu $s1, $s1, $t2
+
+
+
+
+
+# Here:
+
+# addi $s1, $s1, 10
+
+
+
+# beq $zero, $zero, Here
+# addi $t1, $s0, 2
+# subu $s2, $s2, $t1
+
+# Here:
+# addi $t2, $zero, 1
+# subu $s1, $s1, $t2
